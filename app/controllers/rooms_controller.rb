87cde33497956b0234d40e3fe8f028e84057dc51
caroline peron <carolineperon@gmail.com>
2017-11-14 12:25:19 -0200

    added controllers

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,19 @@
+class RoomsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+
+  get 'reservations/new'
+
+  get 'reservations/show'
+
+  get 'reservations/create'
+
+  get 'rooms/new'
+
+  get 'rooms/create'
+
+  get 'rooms/edit'
+
+  get 'rooms/update'
+
+  get 'rooms/destroy'
+
+  get 'rooms/show'
+
+  get 'users/create'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
