796a6bd0fc5f098854d0dd8870b35d38b4772a62
Lucas Furtado <31554554+lambada46@users.noreply.github.com>
2017-11-14 16:40:43 -0200

    deleted param 'seller'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :birthday, :cpf, :password, :address, :seller, :phone_number,  photos: [])
+    params.require(:user).permit(:first_name, :last_name, :birthday, :cpf, :password, :address, :phone_number,  photos: [])
   end
 end
