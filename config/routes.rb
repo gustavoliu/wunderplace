8fb04d5517812e0fbb0d4fe495b7977113dbe97c
Gustavo Liu <gustavoliu@gmail.com>
2017-11-14 16:24:24 -0200

    Overrided Devise's default redirect after new user Sign Up to 'users/edit'. For that, created 'registrations_controller' and changed 'routes.rb'

--- config/routes.rb
+++ config/routes.rb
@@ -19,11 +19,10 @@ Rails.application.routes.draw do
 
   get 'rooms/show'
 
-  # get 'users/create'
-
   resources :users, only: [:edit, :update]
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
