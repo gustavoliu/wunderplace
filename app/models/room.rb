6606ed05cc3781fd995d25b340ce8d76c5bf6efc
lucas furtado <lambada46@gmail.com>
2017-11-15 10:59:06 -0200

    add price to room

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,5 +4,5 @@ class Room < ApplicationRecord
 
   has_many :reservations
   belongs_to :user
-  monetize :price_cents, as: :price # required by 'money-rails' gem
+  monetize :price_centavos, as: :price # required by 'money-rails' gem
 end
