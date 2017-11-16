24ba816797181f2189d4887552fbcdf538d8990a
Pati Demitroff <patidemitroff@gmail.com>
2017-11-16 18:52:41 -0200

    navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar(user, attrs = {})
+    if user.photo != nil
+      cl_image_tag(user.photo.path, attrs)
+    else
+      image_tag("http://placehold.it/50/50", attrs)
+    end
+  end
 end
