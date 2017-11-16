df2f950de7f4f9bc14345ea21c225e7f3a5eb09d
Gustavo Liu <gustavoliu@gmail.com>
2017-11-16 14:57:47 -0200

    Starting to change routes in order to create 'reservations' properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
   resources :reservations
-  resources :rooms
+  # resources :rooms
   resources :reviews
-
-
 
   devise_for :users, controllers: { registrations: "registrations" }, :path => 'devise'
   resources :users, only: [:edit, :update]
+
+  resources :rooms do
+    resources :reservations, only: [:create]
+  end
 
   root to: 'rooms#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
