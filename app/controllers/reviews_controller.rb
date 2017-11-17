8fe7b82a8e6b58128939c992f756521941501d53
caroline peron <carolineperon@gmail.com>
2017-11-17 11:08:54 -0200

    added reservation review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,28 @@
 class ReviewsController < ApplicationController
-  def create
+
+  def new
+    @reservation = Reservation.find(params[:reservation_id])
+    @review = Review.new
   end
+
+  def create
+    @reservation = Reservation.find(params[:reservation_id])
+    @review = Review.new(review_params)
+    @review.reservation = @reservation
+
+    if @review.save
+      redirect_to reservation_path(@reservation)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+
+  end
+
 end
